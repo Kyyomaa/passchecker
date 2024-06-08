569649fec6907d889cc7dd76d0f1c11558b822da
kyoma <zunsure2@gmail.com>
2024-06-08 19:29:15 +0200

    table

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
 use std::io::{self, Write};
-use colored::*;  // Add this line to use the colored crate
+use colored::*;  
+use comfy_table::modifiers::UTF8_ROUND_CORNERS;
+use comfy_table::presets::UTF8_FULL;
+use comfy_table::*;
+use strip_ansi_escapes::strip;
 
 fn main() {
     let mut passwd = String::new();
@@ -11,11 +15,12 @@ fn main() {
     let passwd = passwd.trim();
 
     // Pass by reference to avoid moving the ownership
-    check_password(passwd);
+    let numbers = check_password(passwd);
+    print_vote(passwd, numbers);
 }
 
 // Use &str instead of String to avoid moving ownership
-fn check_password(passwd: &str) {
+fn check_password(passwd: &str) -> &str {
     if passwd.len() > 12 {
         println!("{}", format!("Correct length: {}", passwd.len()).bright_green());
     } else {
@@ -24,9 +29,55 @@ fn check_password(passwd: &str) {
 
     if contains_number(passwd) {
         println!("{}", "Password contains numbers.".bright_green());
+        "YES"
     } else {
         println!("{}", "Password does not contain any numbers.".red());
+        "NO"
     }
+}
+
+fn print_vote(passwd: &str, numbers: &str) {
+    let len_colored = if passwd.len() > 12 {
+        format!("{}", passwd.len()).bright_green()
+    } else {
+        format!("{}", passwd.len()).red()
+    };
+
+    let len_stripped = strip(len_colored.to_string()).unwrap();
+    let len_str = String::from_utf8(len_stripped).unwrap();
+
+    let numbers_colored = if numbers == "YES" {
+        "YES".bright_green()
+    } else {
+        "NO".red()
+    };
+
+    let numbers_stripped = strip(numbers_colored.to_string()).unwrap();
+    let numbers_str = String::from_utf8(numbers_stripped).unwrap();
+
+    let mut table = Table::new();
+    table
+        .load_preset(UTF8_FULL)
+        .apply_modifier(UTF8_ROUND_CORNERS)
+        .set_content_arrangement(ContentArrangement::Dynamic)
+        .set_width(40)
+        .set_header(vec!["CHECKS", "OCCURRENCES", "VOTE"])
+        .add_row(vec![
+            Cell::new("LETTERS").set_alignment(CellAlignment::Center),
+            Cell::new(len_str).set_alignment(CellAlignment::Center),
+            Cell::new("Password length check").set_alignment(CellAlignment::Center),
+        ])
+        .add_row(vec![
+            Cell::new("NUMBERS").set_alignment(CellAlignment::Center),
+            Cell::new(numbers_str).set_alignment(CellAlignment::Center),
+            Cell::new("Contains number check").set_alignment(CellAlignment::Center),
+        ]);
+
+    // Set the default alignment for the third column to right
+    let column = table.column_mut(2).expect("Our table has three columns");
+    column.set_cell_alignment(CellAlignment::Right);
+
+    println!("{table}");
 }
 
 // Function to check if the string contains any numbers
